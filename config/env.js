f525ec927e30583a6c66f5d833eb041ae2ba90da
Dipayan Khaidem <dipayansingha8@gmail.com>
2025-09-14 00:05:37 +0530

    env files and base code setup

---
+++ config/env.js
@@ -0,0 +1,6 @@
+import {config} from 'dotenv';
+config({ path: `.env.${process.env.NODE_ENV || 'development'}.local` }); //since we want multiple .env files for different environments
+//Its called a template string
+export const {PORT,NODE_ENV}=process.env;
+//This way we can switch between our development
+//and production environments without overriding one another
--- eslint.config.js
+++ eslint.config.js
@@ -3,5 +3,15 @@ import globals from "globals";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    languageOptions: {
+      globals: {
+        ...globals.node,     // ✅ add Node.js globals like process, __dirname, etc.
+        ...globals.browser,  // (optional) keep browser globals if you also want frontend code
+      },
+    },
+  },
 ]);
