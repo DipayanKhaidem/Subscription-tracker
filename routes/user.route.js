2213d1beb3d8bc1c6a3618b9803b1b36da5c7c50
Dipayan Khaidem <dipayansingha8@gmail.com>
2025-09-15 21:16:49 +0530

    created mongodb file and connected the db uri

---
+++ routes/user.route.js
@@ -0,0 +1,24 @@
+import {Router} from 'express';
+
+const userRouter=Router();
+
+userRouter.get('/',(req,res)=>{
+    res.send({title:'Get all user details'})
+});
+
+userRouter.get('/:id',(req,res)=>{
+    res.send({title:'Get user details'})
+});
+userRouter.post('/',(req,res)=>{
+    res.send({title:'Create new user'})
+});
+userRouter.put('/:id',(req,res)=>{
+    res.send({
+        title:'Update user details'
+    })
+});
+userRouter.delete('/:id',(req,res)=>{
+    res.send({title:'Delete user'})
+});
+
+export default userRouter;
