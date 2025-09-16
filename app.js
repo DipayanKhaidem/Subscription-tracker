dd66a8705fbd3965ab66cb6482ce3e9434c427fa
Dipayan Khaidem <dipayansingha8@gmail.com>
2025-09-16 20:49:09 +0530

    Created a global error handling middleware

--- app.js
+++ app.js
@@ -4,13 +4,21 @@ import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.routes.js';
 import subscriptionRouter from './routes/subscription.routes.js';
 import connectToDatabase from './Database/mongodb.js';
+import errorMiddleware from './middleware/error.middleware.js';
+import cookieParser from 'cookie-parser';
 
 
 const app=express();
 
+app.use(express.json());//to parse json data in request body
+app.use(express.urlencoded({extended:false}));//this helps to process the form data sent via html forms in simple format
+app.use(cookieParser()); //reads data from incoming requests so that the app can store user data
+
 app.use('/api/v1/users',userRouter);
 app.use('/api/v1/auth',authRouter);
 app.use('/api/v1/subscriptions',subscriptionRouter);
+
+app.use(errorMiddleware);
 
 app.get('/',(req,res)=>{
     res.send("Welcome to the Subscription Tracker API");
