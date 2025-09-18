69fd91a26df0ca7c41cb2ed00f304fdd00509b5e
Dipayan Khaidem <dipayansingha8@gmail.com>
2025-09-18 20:14:04 +0530

    Made the logic for subscription middleware

--- middleware/auth.middleware.js
+++ middleware/auth.middleware.js
@@ -21,6 +21,7 @@ const authorize=async(req,res,next)=>{
         const decoded=jwt.verify(token,JWT_SECRET);
         const user=await User.findById(decoded.userId);
         if(!user) return res.status(401).json({message:"Unauthorized"});
+        req.userId=decoded.userId;
         req.user=user;
         next();
 
